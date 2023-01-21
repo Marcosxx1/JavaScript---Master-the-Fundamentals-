2faf5d1ecda5acd0594851c120cda9a45c3c4139
Marcos Alex <marcosxx1.ma@gmail.com>
2023-01-21 09:12:19 -0400

    Adicionado 6

---
+++ 6.6_JavaScript_in_Node/index.js
@@ -0,0 +1,9 @@
+console.log('Hello World');
+
+/*Aqui estou utilizando o bash CTRL  SHIFT ' 
+
+navegar até a pasta onde estão os arquivos
+Uma dica: cd para entrar é o normal, mas utilize o TAB para autocompletar por exemplo
+    cd ou qualquer comando, depois digite as primeiras letras do nome do diretório ou arquivo e aperte TAB
+    lembrando que no final de diretórios é necessário a /
+*/
