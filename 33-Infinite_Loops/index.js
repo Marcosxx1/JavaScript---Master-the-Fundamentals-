380d821e9bdebf041ab815612b837cb3e9973bdd
Marcos Alex <marcosxx1.ma@gmail.com>
2023-01-23 09:40:35 -0400

    33 e 34

---
+++ 33-Infinite_Loops/index.js
@@ -0,0 +1,15 @@
+ /* Como citei antes, sempre incrementar ou decrementar
+    a variável de controle */
+
+let i = 0;
+
+while ( i < 5) {
+   console.log(i);
+}//SEMPRE COLOQUE O INCREMENTO OU DECREMENTO ou vai rodar para sempre
+ 
+
+
+ 
+
+ 
+ 
---
+++ 34-For...In/index.js
@@ -0,0 +1,13 @@
+/* for in */
+
+const pessoa = { 
+   nome: 'Marcos',
+   idade: 50
+}
+/* É como o for each do java
+   Itera entre todos os items de um conjunto */
+for (let individuo in pessoa) {
+   console.log(individuo);
+}
+ 
+ 
